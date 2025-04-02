07a4c1b458692da7545c672a858630e053277f57
aluno <aluno@mac35.local>
2025-04-02 22:00:03 +0100

    WIP

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const news = {
+    title: "",
+    description: "",
+    date: "",
+}
+
+const drivers_standings = {
+    driver: "",
+    team: "",
+    points: 0,
+    wins: 0,
+    podiums: 0,
+}
+
+const constructors_standings = {
+    team: "",
+    points: 0,
+    wins: 0,
+    podiums: 0,
+}
+
+const postNews = [news];
+const postDriversStandings = [drivers_standings];
+const postContructorsStandings = [constructors_standings];

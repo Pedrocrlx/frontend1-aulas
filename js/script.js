374bea5abfdcba4e4211ff287489bdc80254e8ab
aluno <aluno@mac35.local>
2025-04-03 21:58:10 +0100

    WIP

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 const news = {
     title: "",
     description: "",
-    date: "",
+    date: "03-04-25",
 }
-
 const drivers_standings = {
     driver: "",
     team: "",
@@ -11,13 +10,34 @@ const drivers_standings = {
     wins: 0,
     podiums: 0,
 }
-
 const constructors_standings = {
     team: "",
     points: 0,
     wins: 0,
     podiums: 0,
 }
+
+//JSON EX A
+console.log("----João EX ta aqui----");
+const mock = JSON.stringify(news);
+console.log(mock);
+const json = JSON.parse(mock);
+console.log(json.date);
+console.log("----João EX ta aqui----");
+//JSON EX A
+
+async function fetchNews() {
+    try {
+        const response = await fetch("js/news.json");
+        const data = await response.json();
+        document.getElementById("title").textContent = data.news[0].title;
+        document.getElementById("description").textContent = data.news[0].description;
+        document.getElementById("date").textContent = data.news[0].date;
+    } catch (error) {
+        console.error(error)
+    }
+}
+fetchNews();
 
 const postNews = [news];
 const postDriversStandings = [drivers_standings];

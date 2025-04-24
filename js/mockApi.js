dd72e37d15919d13c3b39063d75dce4f56288f9a
Pedro Santos <pedrosantoscrlx@gmail.com>
2025-04-24 21:57:28 +0100

    header carousel wip

--- js/mockApi.js
+++ js/mockApi.js
@@ -3,16 +3,21 @@ import { getNews } from "../api/api.js";
 document.addEventListener("DOMContentLoaded", async () => {
     const news = await getNews();
     displayNews(news);
+    diplayCarrousel(news);
+    // Initialize the carousel splide.js
+    var splide = new Splide('.splide');
+    splide.mount();
 });
 
-export function displayNews(news) {
-    const newsList = document.getElementById("news_section");
-    news.forEach((post) => {
+
+function diplayCarrousel(news) {
+    const slicedNews = news.slice(0, 3);
+    slicedNews.forEach((post) => {
         const postNews = document.createElement("div");
-        postNews.classList.add("news_card");
+        postNews.classList.add("splide__slide");
         postNews.innerHTML = `
             <div class="image_news">
-                <img src="${post.image}" alt="" class="news_card_image">
+                <img src="${post.image}" class="news_card_image">
             </div>
             <h2 class="news_card_title">${post.title}</h2>
             <p class="news_card_text">${post.description}</p>
@@ -23,9 +28,34 @@ export function displayNews(news) {
             month: "2-digit",
             year: "numeric",
         })
-            }</p >
+            }</p>
+    `;
+        document.querySelector(".splide__list").appendChild(postNews);
+    });
+    console.log(slicedNews, "CAROUSEL NEWS");
+}
+
+export function displayNews(news) {
+    const newsList = document.getElementById("news_section");
+
+    news.forEach((post) => {
+        const postNews = document.createElement("div");
+        postNews.classList.add("news_card");
+        postNews.innerHTML = `
+            <div class="image_news">
+                <img src="${post.image}">
+            </div>
+            <h2 class="news_card_title">${post.title}</h2>
+            <p class="news_card_text">${post.description}</p>
+            <p class="news_card_date">${new Date(
+            post.createdAt
+        ).toLocaleDateString("pt-PT", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+        })
+            }</p>
     `;
         newsList.appendChild(postNews);
     });
 }
-
